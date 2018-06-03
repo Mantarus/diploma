cb70fc1c5b848f2af887020125f9af217fecf441
Ilya Istomin <iistomin@griddynamics.com>
2018-06-03 19:58:17 +0400

    Add Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'persons/profile'
   get 'main/index'
+
+  get 'persons/profile', as: 'user_root'
 
   root 'main#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
