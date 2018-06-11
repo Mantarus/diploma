b07efac48d7c9290f43e1b08334613a022e99113
Ilya Istomin <iistomin@griddynamics.com>
2018-06-11 11:08:57 +0400

    Implement ship rendering

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,66 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+const SHIP_COLOR = "#6f6f6f";
+const WATER_COLOR = "#a2ffff";
+const HIT_COLOR = "#ff3a36";
+const MISS_COLOR = "#ffffff";
+
+function play(game_log) {
+    clear_field();
+
+    let lines = game_log.split(";");
+
+    for (let i = 0; i < lines.length; i++) {
+        let tokens = lines[i].split('\t');
+        if (tokens[0] === "pl") {
+            process_pl_line(tokens)
+        }
+    }
+}
+
+function process_pl_line(tokens) {
+    let player = tokens[1];
+    let ship_idx = parseInt(tokens[2]);
+    let ship_len = parseInt(tokens[3]);
+    let ship_hor = tokens[4] === "true";
+    let ship_y = parseInt(tokens[5]);
+    let ship_x = parseInt(tokens[6]);
+
+    render_ship(player, ship_x, ship_y, ship_len, ship_hor);
+}
+
+function get_cell_id(player, x, y) {
+    return `${player}_${x}_${y}`
+}
+
+function render_ship(player, ship_x, ship_y, ship_len, ship_hor) {
+    console.log([player, ship_x, ship_y, ship_len, ship_hor]);
+
+    let dim;
+    if (ship_hor)
+        dim = [ship_len, 1];
+    else
+        dim = [1, ship_len];
+
+    for (let x = 0; x < dim[0]; x++) {
+        for (let y = 0; y < dim[1]; y++) {
+            console.log([ship_x + x, ship_y + y]);
+            color_cell(player, ship_x + x, ship_y + y, SHIP_COLOR);
+        }
+    }
+
+}
+
+function color_cell(player, x, y, color) {
+    let cell_id = get_cell_id(player, x, y);
+    document.getElementById(`${cell_id}`).style.backgroundColor = color;
+}
+
+function clear_field() {
+    let fields = document.getElementsByClassName("square");
+    for (let i = 0; i < fields.length; i++) {
+        fields[i].style.backgroundColor = WATER_COLOR;
+    }
+}
--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,19 @@ class Game < ApplicationRecord
   belongs_to :user
   has_one :strategy, :foreign_key => 'strategy1_id'
   has_one :strategy, :foreign_key => 'strategy2_id'
+
+  def log_with_linebreaks
+    return nil if not game_log
+
+    arr = game_log.split("\n")
+    new_arr = []
+
+    arr[0..-2].each do |line|
+      new_arr.append(line + ';\\')
+    end
+    new_arr.append(arr[-1])
+
+    inspect
+    new_arr.join
+  end
 end
